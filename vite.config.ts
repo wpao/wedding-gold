eb5fdb2db2971eee870e8a8563b9ed9a5457103c
wpao <wpaozan@gmail.com>
2025-04-28 08:27:37 +0800

    v1.0.0

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "0.0.0.0", // Penting untuk Docker!
+    port: 3003,
+    allowedHosts: ["wedding.albaeud.cyou/gold"], // Tambahkan domain Anda di sini
+  },
+});
