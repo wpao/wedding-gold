4fb002009df62e1a29afdb29c04f934b1a4ba48a
wpao <wpaozan@gmail.com>
2025-05-01 23:36:17 +0800

    v1.0.0 docker server local

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: "http://localhost/",
+  baseURL: "https://api.albaeud.cyou/",
 });
 
 export default axiosInstance;
